e61a116016b12d38915fd3b03d04b971702ee68f
Alper Sakarya <sakaryaa@amazon.com>
2024-02-03 13:50:23 -0500

    Added batch request for buckets with 1000+ objects

--- clean_and_delete_buckets.py
+++ clean_and_delete_buckets.py
@@ -2,8 +2,9 @@
 # Issues a delete for all objects and all versions.
 # Displays a horizontal bar for everfile it's deleting.
 
+
 import boto3
-from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError  
 from tqdm import tqdm
 
 s3 = boto3.client('s3')
@@ -11,13 +12,13 @@ s3 = boto3.client('s3')
 # List buckets
 response = s3.list_buckets()
 
-print("Existing S3 Buckets:")
+print("Existing S3 Buckets:")  
 for i, bucket in enumerate(response['Buckets']):
   print(f"{i}. {bucket['Name']}")
   
-# Get bucket to delete  
+# Get bucket to delete
 bucket_to_delete = input("Enter the number of the bucket to delete: ")
-bucket_to_delete = response['Buckets'][int(bucket_to_delete)]['Name']  
+bucket_to_delete = response['Buckets'][int(bucket_to_delete)]['Name']   
 
 try:
   # Check for Deny policy
@@ -35,12 +36,21 @@ except ClientError as e:
 confirm = input(f"Confirm delete bucket {bucket_to_delete} (y/n): ")
 if confirm.lower() == 'y':
 
-  try:
-    objs = s3.list_objects_v2(Bucket=bucket_to_delete)
+  deleting = True
+  
+  while deleting:
+
+    objs = s3.list_objects_v2(Bucket=bucket_to_delete, MaxKeys=1000)
     
-    if 'Contents' in objs:
+    if 'Contents' in objs and len(objs['Contents']) >= 1000:
+      print("Found more than 1000 objects, making a batch delete request for 1000 objects...")
+      delete_keys = {'Objects': [{'Key': obj['Key']} for obj in objs['Contents']]}
+      s3.delete_objects(Bucket=bucket_to_delete, Delete=delete_keys)
+
+    elif 'Contents' in objs:
+      print("Less than 1000 objects, deleting individually")  
       files = [obj['Key'] for obj in objs['Contents']]
-      
+        
       with tqdm(total=len(files)) as pbar:
         for file in files:
           s3.delete_object(Bucket=bucket_to_delete, Key=file)
@@ -48,11 +58,9 @@ if confirm.lower() == 'y':
           pbar.set_description(f"Deleting {file}")
 
     else:
-      print("No objects to delete")
+      deleting = False
+      print("No objects left to delete")
       
-  except KeyError:
-    print("Error getting objects list, bucket may be empty")
-    
   s3.delete_bucket(Bucket=bucket_to_delete)
 
   print(f"Deleted bucket {bucket_to_delete}")
