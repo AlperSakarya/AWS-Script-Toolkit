d0363437c76114eaece46207ec9383f20c4d45df
Alper Sakarya <sakaryaa@amazon.com>
2024-02-03 13:16:57 -0500

    Added bucket policy check

--- clean_and_delete_buckets.py
+++ clean_and_delete_buckets.py
@@ -3,6 +3,7 @@
 # Displays a horizontal bar for everfile it's deleting.
 
 import boto3
+from botocore.exceptions import ClientError
 from tqdm import tqdm
 
 s3 = boto3.client('s3')
@@ -14,9 +15,22 @@ print("Existing S3 Buckets:")
 for i, bucket in enumerate(response['Buckets']):
   print(f"{i}. {bucket['Name']}")
   
-# Get bucket to delete
+# Get bucket to delete  
 bucket_to_delete = input("Enter the number of the bucket to delete: ")
 bucket_to_delete = response['Buckets'][int(bucket_to_delete)]['Name']  
+
+try:
+  # Check for Deny policy
+  policy = s3.get_bucket_policy(Bucket=bucket_to_delete)
+  if policy['Policy'].find('Deny') != -1:
+    print(f"Bucket {bucket_to_delete} has a Deny policy, quitting.")
+    exit()
+
+except ClientError as e:
+  if e.response['Error']['Code'] == 'NoSuchBucketPolicy':
+    print(f"Bucket {bucket_to_delete} has no policy set.")
+  else:
+    raise
 
 confirm = input(f"Confirm delete bucket {bucket_to_delete} (y/n): ")
 if confirm.lower() == 'y':
